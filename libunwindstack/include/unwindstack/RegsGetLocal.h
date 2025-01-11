c9d9745c4a4a7323a12fa70f773b61a22ec30610
Fuxin Zhang <fxzhang@ict.ac.cn>
2025-01-11 09:20:03 +0800

    use la.local instead of la for loongarch.
    la == la.global will load the content instead of its address, a strange decision

--- libunwindstack/include/unwindstack/RegsGetLocal.h
+++ libunwindstack/include/unwindstack/RegsGetLocal.h
@@ -165,50 +165,7 @@ inline __attribute__((__always_inline__)) void AsmGetRegs(void* reg_data) {
       "st.d $s6, %[base], 232\n"
       "st.d $s7, %[base], 240\n"
       "st.d $s8, %[base], 248\n"
-      "la $t1, 1b\n"
-      "st.d $t1, %[base], 0\n"
-      : [base] "+r"(reg_data)
-      :
-      : "t1", "memory");
-}
-
-#elif defined(__loongarch__)
-
-inline __attribute__((__always_inline__)) void AsmGetRegs(void* reg_data) {
-  asm volatile(
-      "1:\n"
-      "st.d $ra, %[base], 8\n"
-      "st.d $tp, %[base], 16\n"
-      "st.d $sp, %[base], 24\n"
-      "st.d $a0, %[base], 32\n"
-      "st.d $a1, %[base], 40\n"
-      "st.d $a2, %[base], 48\n"
-      "st.d $a3, %[base], 56\n"
-      "st.d $a4, %[base], 64\n"
-      "st.d $a5, %[base], 72\n"
-      "st.d $a6, %[base], 80\n"
-      "st.d $a7, %[base], 88\n"
-      "st.d $t0, %[base], 96\n"
-      "st.d $t1, %[base], 104\n"
-      "st.d $t2, %[base], 112\n"
-      "st.d $t3, %[base], 120\n"
-      "st.d $t4, %[base], 128\n"
-      "st.d $t5, %[base], 136\n"
-      "st.d $t6, %[base], 144\n"
-      "st.d $t7, %[base], 152\n"
-      "st.d $t8, %[base], 160\n"
-      "st.d $r21, %[base], 168\n"
-      "st.d $fp, %[base], 176\n"
-      "st.d $s0, %[base], 184\n"
-      "st.d $s1, %[base], 192\n"
-      "st.d $s2, %[base], 200\n"
-      "st.d $s3, %[base], 208\n"
-      "st.d $s4, %[base], 216\n"
-      "st.d $s5, %[base], 224\n"
-      "st.d $s6, %[base], 232\n"
-      "st.d $s7, %[base], 240\n"
-      "st.d $s8, %[base], 248\n"
-      "la $t1, 1b\n"
+      "la.local $t1, 1b\n"
       "st.d $t1, %[base], 0\n"
       : [base] "+r"(reg_data)
       :
